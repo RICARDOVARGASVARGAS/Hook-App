20f6b64717259fce9fbf4c1c7eb9ac27c772426e
Ricardo Vargas V <vargasvargas.ric@gmail.com>
2025-01-20 17:51:22 -0500

    cap 9 - vid 8

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,6 @@ import { HookApp } from "./HookApp";
 import { CounterApp } from "./components/01-useState/CounterApp";
 import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 import { SimpleForm } from "./components/02-useEffect/SimpleForm";
+import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
 
-createRoot(document.getElementById("root")).render(<SimpleForm />);
+createRoot(document.getElementById("root")).render(<FormWithCustomHook />);
