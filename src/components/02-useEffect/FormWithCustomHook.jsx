d8f329c92b53e189c16b9d514109c63f77da1eb1
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2025-02-17 07:22:10 -0500

    cap9

--- src/components/02-useEffect/FormWithCustomHook.jsx
+++ src/components/02-useEffect/FormWithCustomHook.jsx
@@ -1,23 +1,27 @@
-import React, {  useState } from "react";
+import React, { useEffect } from "react";
 import "./effect.css";
+import { useForm } from "../../hooks/useForm";
 
 export const FormWithCustomHook = () => {
-  const [formState, setFormState] = useState({
+  const [formValues, handleInputChange] = useForm({
     name: "",
     email: "",
     password: "",
   });
 
-  const { name, email, password } = formState;
+  const { name, email, password } = formValues;
 
-  const handleInputChange = ({ target }) => {
-    setFormState({
-      ...formState,
-      [target.name]: target.value,
-    });
+  useEffect(() => {
+    console.log("email cambio");
+  }, [email]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formValues);
   };
+
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <h1>FormWithCustomHook</h1>
       <hr />
 
@@ -55,6 +59,9 @@ export const FormWithCustomHook = () => {
         />
       </div>
 
-    </>
+      <button type="submit" className="btn btn-primary">
+        ENVIAR
+      </button>
+    </form>
   );
 };
