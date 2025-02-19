7689104f793caa497a2271eeaa9e74c77f5ccfee
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2025-02-19 07:28:12 -0500

    cap11

--- src/components/03-examples/MultipleCustomHooks.jsx
+++ src/components/03-examples/MultipleCustomHooks.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import "../02-useEffect/effect.css";
 import { useFetch } from "../../hooks/useFetch";
+import { useCounter } from "../../hooks/useCounter";
 
 export const MultipleCustomHooks = () => {
-  const state = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/1`);
-  console.log(state);
+  const { counter, increment } = useCounter(1);
+  const { loading, data } = useFetch(
+    `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`
+  );
+  // console.log(data);
+  const { author, quote } = !!data && data[0];
 
-  return <div>MultipleCustomHooks</div>;
+  return (
+    <div>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {loading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-2">{quote}</p>
+          <footer className="blockquote-footer">{author}</footer>
+        </blockquote>
+      )}
+
+      <button className="btn btn-primary" onClick={increment}>
+        Siguiente quote
+      </button>
+    </div>
+  );
 };
