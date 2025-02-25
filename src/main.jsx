23be41c9f02b0b62ef2debc3e2bb7bc6e50f63bb
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2025-02-25 07:10:07 -0500

    Learn

--- src/main.jsx
+++ src/main.jsx
@@ -11,5 +11,6 @@ import { Layout } from "./components/05-useLayoutEffect/Layout";
 import { Memorize } from "./components/06-memos/Memorize";
 import { MemoHook } from "./components/06-memos/MemoHook";
 import { CallbackHook } from "./components/06-memos/CallbackHook";
+import { Padre } from "./components/07-tarea-memo/Padre";
 
-createRoot(document.getElementById("root")).render(<CallbackHook />);
+createRoot(document.getElementById("root")).render(<Padre />);
