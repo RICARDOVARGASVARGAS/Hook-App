7351226e7502c79e9f6e12e2a32dfa240842eee2
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2025-03-10 07:31:44 -0500

    Learn

---
+++ src/components/09-useContext/AppRouter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AboutScreen } from "./AboutScreen";
+import { LoginScreen } from "./LoginScreen";
+import { HomeScreen } from "./HomeScreen";
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/about" element={<AboutScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+      </Routes>
+    </Router>
+  );
+};
